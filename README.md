206606c73e32166b5f483e409b1eb1d8806cb3d6
Michael Kuehn <mkuehn10@gmail.com>
2016-07-08 17:53:58 -0400

    Clean up code & add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Walt Disney World "Neighborhood Map"
+
+## How to run the app
+Link to project is [here](https://mkuehn10.github.io/portfolio/neighborhood)
+
+Click on the various markers to see more information.
+
+The search box can be used to filter the visible markers.
+
+## References
+https://www.mediawiki.org/wiki/API:Opensearch
+http://stackoverflow.com/questions/17961669/how-to-make-a-full-screen-google-map-with-website-menu-overlay
+http://knockoutjs.com/documentation/introduction.html
+http://stackoverflow.com/questions/20857594/knockout-filtering-on-observable-array
--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+// initialLocations contains the information on the five locations
 var initialLocations = [{
     streetAddress: 'Cinderella Castle',
     city: 'Walt Disney World',
@@ -25,6 +26,10 @@ var initialLocations = [{
     latlng: [28.417988, -81.583436]
 }];
 
+/* Data Model Location
+ * This object stores all of the data for the locations. The Wikipedia API is
+ * used to obtain information for each location.
+ */
 var Location = function(data, map) {
     var self = this;
     this.streetAddress = ko.observable(data.streetAddress);
@@ -41,7 +46,7 @@ var Location = function(data, map) {
     }, this);
 
     // Get wikipedia article information and link using
-    //https://www.mediawiki.org/wiki/API:Opensearch
+    // https://www.mediawiki.org/wiki/API:Opensearch
     self.params = $.param({
         'action': 'opensearch',
         'search': self.streetAddress,
@@ -63,8 +68,11 @@ var Location = function(data, map) {
                     };
                 }
                 self.infoWindow = new google.maps.InfoWindow({
-                    content: '<div class="info-window"><h3>' + self.wikiInfo.title +
-                        '</h3><br><p>' + self.wikiInfo.leadPara + '</p><br><a href="' + self.wikiInfo.href + '" target="new">More Info...</a></div>'
+                    content: '<div class="info-window"><h3>' +
+                        self.wikiInfo.title +
+                        '</h3><p>' + self.wikiInfo.leadPara + '</p><a href="' +
+                        self.wikiInfo.href +
+                        '" target="new">More Info...</a></div>'
                 });
                 self.infoWindow.addListener('closeclick', function() {
                     self.locationClicked();
@@ -74,22 +82,27 @@ var Location = function(data, map) {
         .error(function() {
             alert("Error loading Wikipedia data.  Please try again later.");
         });
-
+    // Set up the location's marker using the data from the initialLocations
+    // object
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(this.latlng()[0], this.latlng()[1]),
         animation: google.maps.Animation.DROP,
         title: this.completeAddress()
     });
 
+    // Keeps track of whether or not this Location's Info Window is open
     this.infoOpen = false;
 
     marker.addListener('click', function() {
         self.locationClicked();
     });
 
+    // Function that processes clicking on the marker or the location
+    // in the list view.
     this.locationClicked = function() {
-        //console.log(marker.open + "marker")
         if (!self.infoOpen) {
+            map.setZoom(20);
+            map.panTo(marker.position);
             self.infoWindow.open(map, marker);
             self.infoOpen = true;
             marker.setAnimation(google.maps.Animation.BOUNCE);
@@ -97,8 +110,14 @@ var Location = function(data, map) {
             self.infoWindow.close();
             self.infoOpen = false;
             marker.setAnimation(null);
+            map.setZoom(16);
+            map.fitBounds(new google.maps.LatLngBounds(
+                new google.maps.LatLng(28.415051, -81.586766),
+                new google.maps.LatLng(28.422288, -81.576917)));
         }
     };
+
+    // Controls the visibility of the marker based on the search filter
     self.isVisible = ko.observable(false);
 
     self.isVisible.subscribe(function(currentState) {
@@ -109,6 +128,7 @@ var Location = function(data, map) {
         }
     });
 
+    // Default is that the marker is visible
     self.isVisible(true);
 
 
@@ -117,13 +137,15 @@ var Location = function(data, map) {
 var ViewModel = function() {
     var self = this;
 
+    // Set up the Google Map
     var mapOptions = {
         center: new google.maps.LatLng(28.419552, -81.582196),
         disableDefaultUI: true,
         mapTypeId: google.maps.MapTypeId.SATELLITE,
-        zoom: 15
+        zoom: 16
     };
-    var googleMap = new google.maps.Map(document.querySelector('#map'), mapOptions);
+    var googleMap = new google.maps.Map(document.querySelector('#map'),
+                                        mapOptions);
 
     if (!googleMap) {
         alert("Issue loading Google Maps.  Please try again later.");
@@ -133,25 +155,27 @@ var ViewModel = function() {
         new google.maps.LatLng(28.415051, -81.586766),
         new google.maps.LatLng(28.422288, -81.576917));
 
+    // Create an array of the locations
     self.locationList = ko.observableArray([]);
 
     initialLocations.forEach(function(locationItem) {
         self.locationList().push(new Location(locationItem, googleMap));
     });
 
-    self.currentLocation = ko.observable(this.locationList()[0]);
-
+    // Stores the value of the search box
     self.query = ko.observable('');
 
+    // filteredItems contains the locations that meet the search criteria
     self.filteredItems = ko.computed(function() {
         var filter = self.query().toLowerCase();
         return ko.utils.arrayFilter(self.locationList(), function(item) {
-            var match = item.completeAddress().toLowerCase().indexOf(filter.toLowerCase()) >= 0;
+            var match = item.streetAddress().toLowerCase().indexOf(filter.toLowerCase()) >= 0;
             item.isVisible(match);
             return match;
         });
     });
 
+    // Helps make the Google Map responsive to window resizing
     window.addEventListener('resize', function(e) {
         var center = googleMap.getCenter();
         googleMap.setCenter(center);
@@ -159,6 +183,6 @@ var ViewModel = function() {
     });
 };
 
-
+// Invoke the app
 var ViewModel = new ViewModel();
 ko.applyBindings(ViewModel);
